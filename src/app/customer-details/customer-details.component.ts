72af970aea0f015099f818e6887832857fbda5ee
Ameeya <crazy.ameeyadas@gmail.com>
2020-07-31 11:59:30 +0530

    template driven form and validation

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-customer-details',
   templateUrl: './customer-details.component.html',
   styleUrls: ['./customer-details.component.css']
 })
-export class CustomerDetailsComponent implements OnInit {
+export class CustomerDetailsComponent implements OnInit, OnDestroy {
+
+  customer = {
+    name: null,
+    gender: null,
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Customer Details Component initialized.");
+  }
+
+  ngOnDestroy(): void {
+    console.log("Customer Details component goes out of scope.");
+  }
+
+  onSubmit(): void {
+    console.log("customer: ", this.customer);
   }
 
 }
--- src/app/customer-details/customer-details.module.ts
+++ src/app/customer-details/customer-details.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerDetailsComponent } from './customer-details.component';
 
@@ -13,6 +14,7 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes),
   ],
 })
