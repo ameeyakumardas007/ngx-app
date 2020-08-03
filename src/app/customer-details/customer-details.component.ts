c54f15f982f6363ad289567376a6e4702e47bd7e
Ameeya <crazy.ameeyadas@gmail.com>
2020-08-03 12:02:16 +0530

    Reactive Forms, built-in validator, custom validators

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-details',
@@ -7,15 +8,26 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class CustomerDetailsComponent implements OnInit, OnDestroy {
 
-  customer = {
-    name: null,
-    gender: null,
-  };
+  customerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
     console.log("Customer Details Component initialized.");
+
+    // build your form group
+    this.customerForm = this._formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(5)]],
+      age: [null, [this.minimumAgeValidator, this.numberValidator, Validators.max(50)]],
+    });
+
+    // change of field detection
+    this.customerForm.get('name').valueChanges.subscribe(
+      value => console.log("name changed to: ", value)
+    );
+
   }
 
   ngOnDestroy(): void {
@@ -23,7 +35,27 @@ export class CustomerDetailsComponent implements OnInit, OnDestroy {
   }
 
   onSubmit(): void {
-    console.log("customer: ", this.customer);
+    console.log("customer: ", this.customerForm.value);
+  }
+
+  minimumAgeValidator(control: AbstractControl): {[key: string]: boolean} | null {
+    if (control.value < 18) {
+      return {
+        invalidAge: true,
+      };
+    } else {
+      return null;
+    }
+  }
+
+  numberValidator(control: AbstractControl): {[key: string]: boolean} | null {
+    if (isNaN(control.value)) {
+      return {
+        isNotANumber: true,
+      };
+    } else {
+      return null;
+    }
   }
 
 }
--- src/app/customer-details/customer-details.module.ts
+++ src/app/customer-details/customer-details.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerDetailsComponent } from './customer-details.component';
 
@@ -15,6 +15,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes),
   ],
 })
